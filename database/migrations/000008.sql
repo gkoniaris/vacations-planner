a9aa1a5ae581bb71b74d04cef3ec2ea5ec140e7c
George Koniaris <koniaris.giwrgos@gmail.com>
2020-08-01 23:11:31 +0300

    Change total_employees enum from 0-9 to 1-9

--- database/migrations/000008.sql
+++ database/migrations/000008.sql
@@ -5,7 +5,7 @@ CREATE TABLE `company` (
   `country` varchar(100) NOT NULL,
   `city` varchar(100) NOT NULL,
   `industry_id` mediumint(4) unsigned NOT NULL,
-  `total_employees` enum('0 - 9','10 - 50','51 - 200','201 - 1000','1000+') NOT NULL,
+  `total_employees` enum('1 - 9','10 - 50','51 - 200','201 - 1000','1000+') NOT NULL,
   PRIMARY KEY (`id`),
   KEY `fk_company_industry_id_idx` (`industry_id`),
   CONSTRAINT `fk_company_industry_id` FOREIGN KEY (`industry_id`) REFERENCES `industries` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION
